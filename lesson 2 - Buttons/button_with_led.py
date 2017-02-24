8f424cebc3e6425808da42d8a574cf4a2b24ca01
michael luckenbill <michael.luckenbill@gmail.com>
2017-02-23 21:26:54 -0500

    added new button switch for the lights

--- lesson 2 - Buttons/button_with_led.py
+++ lesson 2 - Buttons/button_with_led.py
@@ -1,28 +1,39 @@
-from gpiozero import Button
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+light_switch_on = False
 
 def led_on():
-        print("LED On")
+        
         GPIO.output(17, GPIO.HIGH)
 def led_off():
-        print("LED Off")
+        
         GPIO.output(17, GPIO.LOW)
 
-try:
 
-        print('Press ctrl + c to stop and exit')
+try:
+        print ('Press ctrl+c to exit')
+
         while True:
-                print('Press button to turn on lights')
-                button.wait_for_press()
-                led_on()
-                print('Press button to turn off lights')
-                button.wait_for_press()
+                input_state = GPIO.input(2)
+
+                if input_state == False:
+                        print('Button Pressed')
+
+                        time.sleep(0.2)
+                        
+                        if light_switch_on == False:
+                                led_on()
+                                light_switch_on = True
+                        else:
+                                led_off()
+                                light_switch_on = False
 except KeyboardInterrupt:
-        print("Exiting Application")
+        print("Turning off the lights now")
         led_off()
         GPIO.cleanup()
