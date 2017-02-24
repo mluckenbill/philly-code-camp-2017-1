1ec5d3055c4478600ad19d3ca9ec1f47f1cb8aea
michael luckenbill <michael.luckenbill@gmail.com>
2017-02-23 21:43:41 -0500

    updated script

--- lesson 2 - Buttons/button_hard.py
+++ lesson 2 - Buttons/button_hard.py
@@ -6,10 +6,8 @@ GPIO.setmode(GPIO.BCM)
 # here we complete the circuit when the button is pushed  we do this on GPIO Pin 2
 # key to note is normally 3.3V is sent out of GPIO pins
 # however, each GPIO pin has software configurable pull-up and pull-down resistors.
-
 # When using a GPIO pin as an input, you can configure these resistors
 # so that one or either or neither of the resistors is enabled, using the optional pull_up_down parameter to GPIO.setup
-
 # If it is set to GPIO.PUD_UP, the pull-up resistor is enabled; if it is set to GPIO.PUD_DOWN, the pull-down resistor is enabled.
 
 GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -24,5 +22,4 @@ try:
 
 except KeyboardInterrupt:
         print("Exiting Application")
-        led_off()
         GPIO.cleanup()
