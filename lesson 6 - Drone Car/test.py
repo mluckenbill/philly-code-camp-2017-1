14b1c587782a1fcf8f918f2a496946de51f9e3d7
michael mastermluck luckenbill <michael.luckenbill@gmail.com>
2017-02-24 02:56:11 -0500

    updated

--- lesson 6 - Drone Car/test.py
+++ lesson 6 - Drone Car/test.py
@@ -79,42 +79,52 @@ def RobotSpinRight():
   GPIO.output(LEFT_WHEEL_FORWARD, GPIO.HIGH)
   GPIO.output(LEFT_WHEEL_REVERSE, GPIO.LOW)
 
-RobotStop()
-count=0
-while True:
- i=0
- avgDistance=0
- for i in range(5):
-  GPIO.output(TRIG, False)                 #Set TRIG as LOW
-  time.sleep(0.1)                                   #Delay
-  GPIO.output(TRIG, True)                  #Set TRIG as HIGH
-  time.sleep(0.00001)                           #Delay of 0.00001 seconds
-  GPIO.output(TRIG, False)                 #Set TRIG as LOW
-  pulse_start = time.time()
-  pulse_end = time.time()
-  pulse_duration = pulse_end - pulse_start #time to get back the pulse to sensor
-  distance = pulse_duration * 17150        #Multiply pulse duration by 17150 (34300/2) to get distance
-  distance = round(distance,2)                 #Round to two decimal points
-  avgDistance=avgDistance+distance
- avgDistance=avgDistance/5
- print avgDistance
- flag=0
- if avgDistance < 15:      #Check whether the distance is within 15 cm range
-    count=count+1
-    RobotStop()
-    time.sleep(1)
-    RobotReverse()
-    time.sleep(1.5)
-    if (count%3 ==1) & (flag==0):
-     RobotRight()
-     flag=1
-    else:
-     RobotLeft()
-     flag=0
-    time.sleep(1.5)
-    RobotStop()
-    time.sleep(1)
- else:
-    RoboForward()
-    flag=0
-  
+
+
+try:
+  RobotStop()
+  count=0
+  while True:
+   i=0
+   avgDistance=0
+   for i in range(5):
+    GPIO.output(TRIG, False)                 #Set TRIG as LOW
+    time.sleep(0.1)                                   #Delay
+    GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+    time.sleep(0.00001)                           #Delay of 0.00001 seconds
+    GPIO.output(TRIG, False)                 #Set TRIG as LOW
+    pulse_start = time.time()
+    pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start #time to get back the pulse to sensor
+    distance = pulse_duration * 17150        #Multiply pulse duration by 17150 (34300/2) to get distance
+    distance = round(distance,2)                 #Round to two decimal points
+    avgDistance=avgDistance+distance
+   avgDistance=avgDistance/5
+   print avgDistance
+   flag=0
+   if avgDistance < 15:      #Check whether the distance is within 15 cm range
+      count=count+1
+      RobotStop()
+      time.sleep(1)
+      RobotReverse()
+      time.sleep(1.5)
+      if (count%3 ==1) & (flag==0):
+       RobotRight()
+       flag=1
+      else:
+       RobotLeft()
+       flag=0
+      time.sleep(1.5)
+      RobotStop()
+      time.sleep(1)
+   else:
+      RoboForward()
+      flag=0
+
+except KeyboardInterrupt:
+
+# CTRL+C exit, turn off the drives and release GPIO pins
+  print "Stopped early, please turn off my power!"
+  RobotStop()
+  GPIO.cleanup()
+
