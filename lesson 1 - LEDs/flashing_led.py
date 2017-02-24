6ff796157fb13b0ad729358ad3d38b57755d39e4
michael luckenbill <michael.luckenbill@gmail.com>
2017-02-23 20:53:19 -0500

    updated the flasing script so it works on raspberry pi 3

--- lesson 1 - LEDs/flashing_led.py
+++ lesson 1 - LEDs/flashing_led.py
@@ -5,25 +5,22 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
 
-def led_on()
-	print "LED on"
-	GPIO.output(17,GPIO.HIGH)
-
-def led_off()
-	print "LED off"
-	GPIO.output(17,GPIO.LOW)
+def led_on():
+        print("LED On")
+        GPIO.output(17, GPIO.HIGH)
+def led_off():
+        print("LED Off")
+        GPIO.output(17, GPIO.LOW)
 
 try:
-	print 'Press ctrl + c to stop and exit'
+        print('Press ctrl + c to stop and exit')
 
-	while True:
-		led_on()
-	    sleep(1)
-	    led_of()
-	    sleep(1)
-
+        while True:
+                led_on()
+                time.sleep(1)
+                led_off()
+                time.sleep(1)
 except KeyboardInterrupt:
-    print "Turning off the lights now"
-    
-    led_off()
-	GPIO.cleanup()
+        print("Turning off the lights now")
+        led_off()
+        GPIO.cleanup()
